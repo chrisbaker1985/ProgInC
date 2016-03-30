30232b53f6dfb0560a329faf215cb68f38e0ea6d
Chris Baker <chrisbaker1985@gmail.com>
2016-03-30 19:49:07 -0400

    Chapter 9 exercises complete

--- Ch9/ex13.c
+++ Ch9/ex13.c
@@ -1,8 +1,36 @@
-// Description of the program
+/* Program that converts all characters in a string to upper case
+ * using formula c - 'a' + 'A'										*/
 
 #include <stdio.h>
 
 int main(void)
 {
+	void convertStringToUpper(char string[]);
+	char string[] = "There once was a girl named Alesia";
+	
+	printf("%s\n", string);
+	
+	convertStringToUpper(string);
+	
+	printf("%s\n", string);
 	return 0;
+}
+
+char toUpperCase(char c)
+{	
+	return c - 'a' + 'A';
+}
+
+void convertStringToUpper(char string[])
+{
+	int i = 0;
+	char toUpperCase(char c);
+	while(string[i] != '\0')
+	{
+		if(string[i] >= 'a' && string[i] <= 'z')
+		{
+			string[i] = toUpperCase(string[i]);
+		}
+		i++;
+	}
 }
--- Ch9/ex14.c
+++ Ch9/ex14.c
@@ -1,8 +1,61 @@
-// Description of the program
+// Program that converts integers to strings
 
 #include <stdio.h>
+#include <stdbool.h>
 
 int main(void)
 {
+	void intToString(int n, char returnArray[]);
+	char returnArray[50] = "";
+	int a = -752;
+	printf("Starting: %i\n", a);
+	
+	intToString(a, returnArray);
+	
+	printf("As string: %s\n", returnArray);
+	
 	return 0;
+}
+
+void intToString(int n, char returnArray[])
+{
+	char tempArray[50] = "";
+	char intToChar(int n);
+	int i = 0, j = 0;
+	bool isNegative = false;
+	if(n < 0)
+	{
+		isNegative = true;
+		n *= -1;
+	}
+	
+	while(n > 0)
+	{
+		tempArray[i] = intToChar(n % 10);
+		n /= 10;
+		i++;
+	}	
+	
+	if(isNegative)
+	{
+		tempArray[i] = '-';
+	} 
+	else
+	{
+		i--;
+	}	
+
+	while(i >= 0)
+	{		
+		returnArray[j] = tempArray[i];
+		i--;
+		j++;		
+	}
+	
+	returnArray[j] = '\0';
+}
+
+char intToChar(int n)
+{
+	return (char)(n + '0');
 }
